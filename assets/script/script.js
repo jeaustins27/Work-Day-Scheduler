49bc6b1189dc99187b938fb49678d8b889df421a
Jeaustin <JeaustinS27@gmail.com>
2023-02-01 18:28:01 -0500

    created event handler for savBtn and function for hour checker

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,50 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+    // When any save button is clicked, it will take your input and the corresponding time block and save that data in local storage.
+    $('.saveBtn').on('click', function () {
+        var task = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+
+        // saving your input into local storage with the corresponding time-block
+        localStorage.setItem(time, task);
+    });
+
+// function to check the current hour to correctly color code the time block to match past, present, and future
+function taskChecker() {
+    var currentHour = dayjs().hour();
+
+    // looping through each time block 
+    $('.time-block').each(function () {
+        var timeBlock = parseInt($(this).attr("id").split('-')[1]);
+
+        // checking the current time against the timeblock to add the correct class for styling.
+        if (currentHour > timeBlock) {
+            $(this).addClass('past');
+            } else if (currentHour < timeBlock) {
+                $(this).addClass('future');
+                $(this).removeClass('past');
+            } else {
+                $(this).addClass('present');
+                $(this).removeClass('past');
+                $(this).removeClass('future');
+            };
+        });
+    };
+
+// running the check that loops through each time block to correctly style each time block.
+taskChecker();
+
+// Getting the stored tasks from local storage that corresponds with each time block.
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+
+// Displays current day in the header
+$('#currentDay').text(dayjs().format('[Today is] dddd, MMMM D, YYYY[.]'));
+});
